d51e9f648ba44477a4cb3af83200e6860ab5294d
Raunak Kesarwani <raunakkesarwani1709@gmail.com>
2026-07-07 10:28:53 +0530

    fix(router): correct application routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './routes/Home.js';
 import About from './routes/About.js';
 import Projects from './routes/Projects.js';
@@ -7,15 +7,12 @@ import Contact from './routes/Contact.js';
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
